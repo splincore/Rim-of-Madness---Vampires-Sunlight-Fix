47af3b2154c44d8e1b3b6f962a0032861d88572c
splincore <splincore@web.de>
2019-11-22 16:29:14 +0100

    Initial commit

---
+++ Source/ROMVSunlightFix/HarmonyPatch.cs
@@ -0,0 +1,37 @@
+﻿using Verse;
+using Harmony;
+using RimWorld;
+using Vampire;
+using System.Collections.Generic;
+
+namespace ROMVSunlightFix
+{
+    [StaticConstructorOnStartup]
+    static class HarmonyPatch
+    {
+        static HarmonyPatch()
+        {
+            var harmony = HarmonyInstance.Create("rimworld.carnysenpai.romvsunlightfix");
+            harmony.Patch(AccessTools.Method(typeof(Scenario), "Notify_PawnGenerated", null, null), null, new HarmonyMethod(typeof(HarmonyPatch).GetMethod("Notify_PawnGenerated_PostFix")), null);
+        }
+
+        [HarmonyPostfix]
+        public static void Notify_PawnGenerated_PostFix(Pawn pawn)
+        {
+            if (pawn.IsVampire())
+            {
+                Map currentMap = Find.CurrentMap;
+                if (currentMap == null) return;
+                if (VampireUtility.IsDaylight(currentMap) && pawn.Faction != Faction.OfPlayerSilentFail)
+                {
+                    pawn.health.hediffSet.hediffs.RemoveAll(h=> h.def == VampDefOf.ROM_Vampirism);
+                    Dictionary<Pawn, int> recentVampires = Find.World.GetComponent<WorldComponent_VampireTracker>().recentVampires;
+                    if (recentVampires != null && recentVampires.ContainsKey(pawn))
+                    {
+                        recentVampires.Remove(pawn);
+                    }
+                }
+            }
+        }
+    }
+}
